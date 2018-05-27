af06c020abfd2825a45464327981d6063319d692
febriardis <ardisaputrafebri@gmail.com>
2018-05-28 04:23:29 +0700

    edit-db

--- config/database.php
+++ config/database.php
@@ -19,8 +19,9 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
-
+    'default' => env('DB_CONNECTION', 'pgsql'),
+    //'default' => env('DB_CONNECTION', 'mysql'),
+    
     /*
     |--------------------------------------------------------------------------
     | Database Connections
